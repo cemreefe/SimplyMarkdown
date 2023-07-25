40abb910dbb6d3b949a7aa63e9d136d912f78710
Cemre <44120900+cemreefe@users.noreply.github.com>
2023-07-25 17:38:51 +0000

    Fix subdir preview

--- subdirectory_helpers.py
+++ subdirectory_helpers.py
@@ -8,7 +8,7 @@ class PreviewExtension(markdown.extensions.Extension):
 
     def __init__(self, base_path=None, processor=None, **kwargs):
         self.config = {
-            'preview_limit': [10, "The number of components to show in the preview"]
+            'preview_limit': [5, "The number of components to show in the preview"]
         }
         self.base_path = base_path
         self.processor = processor
@@ -37,32 +37,55 @@ class PreviewBlockProcessor(markdown.blockprocessors.BlockProcessor):
     def run(self, parent, blocks):
         block = blocks.pop(0)  # Get the special tag line
         self.directory_name = re.match(r'^%\s*([^>]+)$', block).group(1).strip()
-        content = self.get_preview_content()
-        div = ET.Element('div', attrib={'class': 'postPreview'})
-        div.text = content
-        parent.append(div)
+        contents, dates, hrefs = self.get_preview_content()
+
+        for content, date, href in zip(contents, dates, hrefs):
+
+            # Create the child <div> element for the date with the class 'previewDate'
+            date_div = ET.Element('div', attrib={'class': 'previewDate'})
+            date_div.text = date
+
+            text_div = ET.Element('div')
+            text_div.text = content
+
+            wrapper = ET.Element('div', attrib={'class': 'postPreview'})
+            wrapper.append(date_div)
+            wrapper.append(text_div)
+
+            # Create the anchor (<a>) element with the provided href
+            a = ET.Element('a', attrib={'href': href, 'class':'previewHref'})
+            a.append(wrapper)
+
+            parent.append(a)
 
     def get_preview_content(self):
         if not self.directory_name:
             return ''
 
-        content = ""
         if self.base_path:
             directory_path = os.path.join(self.base_path, self.directory_name)
         else:
             directory_path = self.directory_name
 
+        contents, dates, hrefs = [], [], []
+
         if os.path.exists(directory_path) and os.path.isdir(directory_path):
             for root, _, files in os.walk(directory_path):
+                relpath = os.path.relpath(root, directory_path)
                 for file in files:
+                    date = relpath
+                    href = ('./' + relpath + '/' + os.path.splitext(file)[0].replace(', ', '-').replace(' ', '-'))
                     if file.lower().endswith('.md'):
                         file_path = os.path.join(root, file)
                         with open(file_path, 'r') as md_file:
                             file_content = md_file.read().strip()
                             components = file_content.split('\n\n')[:self.preview_limit]
-                            content += '\n\n'.join(components) + '\n\n'
+                            content = '\n\n'.join(components) + '\n\n'
                             content = self.processor(content)
+                            contents.append(content)
+                            dates.append(date)
+                            hrefs.append(href)
 
-        return content
+        return contents, dates, hrefs
 
 
