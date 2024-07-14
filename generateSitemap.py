05911e6fc061e823bef751a5b5b1c253085a9702
Cemre <cemreefe@gmail.com>
2024-07-14 19:44:42 +0000

    Respect canonical_url in the sitemap

--- generateSitemap.py
+++ generateSitemap.py
@@ -1,4 +1,5 @@
 import os
+from bs4 import BeautifulSoup
 
 def generate_sitemap(root_directory, urlroot=''):
     def get_html_files(directory):
@@ -10,19 +11,33 @@ def generate_sitemap(root_directory, urlroot=''):
                     html_files.append(file_path)
         return html_files
 
+    def get_canonical_url(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+            canonical_link = soup.find('link', rel='canonical')
+            if canonical_link and canonical_link.has_attr('href'):
+                return canonical_link['href']
+        return None
+
     html_files = get_html_files(root_directory)
 
     output_file = os.path.join(root_directory, 'sitemap.xml')
 
-    with open(output_file, 'w') as sitemap_file:
+    with open(output_file, 'w', encoding='utf-8') as sitemap_file:
         sitemap_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         sitemap_file.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 
         for file_path in html_files:
-            url = file_path.replace(root_directory, urlroot + '/').replace('\\', '/').lstrip('/')
-            url = url.replace('//', '/')
-            url = url.replace('https:/', 'https://')
-            url = url.replace(".html", "")
+            canonical_url = get_canonical_url(file_path)
+            if canonical_url:
+                url = (urlroot + canonical_url).replace('//', '/')
+                url = url.replace('https:/', 'https://')
+            else:
+                url = file_path.replace(root_directory, urlroot + '/').replace('\\', '/').lstrip('/')
+                url = url.replace('//', '/')
+                url = url.replace('https:/', 'https://')
+                url = url.replace(".html", "")
+
             sitemap_file.write(f'  <url>\n')
             sitemap_file.write(f'    <loc>{url}</loc>\n')
             sitemap_file.write(f'  </url>\n')
